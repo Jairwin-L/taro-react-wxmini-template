545745d9d2157362b1845684e755b43b7b67277b
Jairwin <jairwin@jairwin.cn>
2023-03-22 21:14:13 +0800

    feat: 增加错误边界处理

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from 'react';
+import { useError } from '@tarojs/taro';
 import './app.scss';
 
 const App: React.FC<{ children: ReactNode }> = ({ children }) => {
+  useError((error) => console.log(`onError----->`, error));
   return <>{children}</>;
 };
 
